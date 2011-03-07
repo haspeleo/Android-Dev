5c387db260887e437243dcde173a9d7751479fba
haspeleo <hassen.doghmen@gmail.com>
2011-03-07 13:43:03 +0100

    add io c++

---
+++ FILE_IO/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: doghmen
+ *
+ * Created on 7 mars 2011, 13:42
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
