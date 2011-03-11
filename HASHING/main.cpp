d47bab9b13a1fb293f55be6cfe7f61d09d011eb8
haspeleo <hassen.doghmen@gmail.com>
2011-03-11 21:10:38 +0100

    hashing

---
+++ HASHING/main.cpp
@@ -0,0 +1,38 @@
+/* 
+ * File:   main.cpp
+ * Author: doghmen
+ *
+ * Created on 11 mars 2011, 15:32
+ * http://www.brpreiss.com/books/opus4/
+ * http://www.mysqlperformanceblog.com/2006/06/09/why-mysql-could-be-slow-with-large-tables/
+ */
+
+#include <cstdlib>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int hash_folding(string word, int tableSize) {
+
+    int sum = 0;
+    for(int i = 0; i < word.length(); i++) {
+        sum += word[i];
+        cout <<word[i]<<" ";
+    }
+    return sum % tableSize;
+}
+
+
+int main(int argc, char** argv) {
+
+    string s1 = "ABC";
+    string s2 = "CZB";
+    int i1 = hash_folding(s1, 100);
+    int i2 = hash_folding(s2, 100);
+
+    cout <<"Hashing values for "<<s1<<" - "<<s2<<" are : "<<i1<<" - "<<i2<<endl;
+
+    return 0;
+}
+
