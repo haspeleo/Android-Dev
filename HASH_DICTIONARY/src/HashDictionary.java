8479eb2bb7ebdbb0cdd441926567d5c5e88032a9
haspeleo <hassen.doghmen@gmail.com>
2011-03-13 17:25:03 +0100

    dictonary with hashing

---
+++ HASH_DICTIONARY/src/HashDictionary.java
@@ -0,0 +1,46 @@
+
+import java.util.Dictionary;
+import java.util.Enumeration;
+
+/*
+
+ */
+public class HashDictionary <K extends Comparable <? super K>, E>
+                            extends Dictionary <K, E>{
+
+    @Override
+    public int size() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Enumeration<K> keys() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Enumeration<E> elements() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public E get(Object key) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public E put(K key, E value) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public E remove(Object key) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
---
+++ HASH_DICTIONARY/src/main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+
+
+ * A practical introduction to data structures and Algorithms analysis //book
+ * http://www.brpreiss.com/books/opus4/
+ */
+
+/**
+ *
+ * @author doghmen
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Dictionary implementation using a hashtable");
+        // TODO code application logic here
+    }
+
+}
