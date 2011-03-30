be9e4037f55640aca74a811515cab7ca9da10aad
doghmen <doghmen@doghmen-laptop.(none)>
2011-03-30 09:45:41 +0200

    graph adding functions

--- GRAPHS/main.cpp
+++ GRAPHS/main.cpp
@@ -24,15 +24,69 @@ typedef struct {
   int nvertices;
   int nedges;
   bool directed;
-};
+} graph;
+
+void initializeGraph(graph *g, bool directed) {
+    g->nvertices = 0;
+    g->nedges = 0;
+    g->directed = directed;
+
+    for(int i = 1; i < MAXV; i++) {
+        g->degree[i] = 0;
+    }
+
+    for(int i = 1; i < MAXV; i++) {
+        g->edges[i] = NULL;
+    }
+}
 
 
 
 
+void readGraph(graph *g, bool directed) {
+    int m;
+    int x, y;
 
+    initializeGraph(g, directed);
 
+    cin >> g->nvertices >> m;
+    for(int i = 0; i <  m; i++) {
+        cin >> x >> y;
+        insertEdge(g, x, y, directed);
+    }
+}
 
+void insertEdge(graph *g, int x, int y, bool directed) {
+    
+    edgenode *p;
+    p = malloc(sizeof(struct edgenode));
+    p->y = y;
+    p->weight = NULL;
+    p->next = g->degree[x];
 
+    g->edges[x] = p;
+    g->degree[x] ++;
+
+    if(directed == false)
+        insertEdge(g, x, y, true);
+    else
+        g->nedges ++;
+}
+
+void printGraph(graph *g) {
+
+    edgenode *p;
+    for(int i = 0; i < g->nvertices; i++) {
+        cout <<i<<": "<<endl;
+        p = g->edges[i];
+
+        while(!p) {
+            cout <<p->y<<endl;
+            p = p->next;
+        }
+        cout <<" "<<endl;
+    }
+}
 
 int main(int argc, char** argv) {
 
