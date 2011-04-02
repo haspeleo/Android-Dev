477e49541ed418f53225b60bb20bb7bb52b70ccc
haspeleo <hassen.doghmen@gmail.com>
2011-04-02 18:34:48 +0200

    stack methods

--- STACKS/Stack.cpp
+++ STACKS/Stack.cpp
@@ -8,6 +8,8 @@
 #include <cstdlib>
 #include "Stack.h"
 
+#define MAX_SIZE 1000
+
 template <class ItemType>
 Stack<ItemType>::Stack() {
     counter = 0;
@@ -21,39 +23,51 @@ Stack<ItemType>::Stack(const Stack& orig) {
 
 template <class ItemType>
 Stack<ItemType>::~Stack() {
+    delete head;
+    counter = 0;
 }
 
 template <class ItemType>
 ItemType Stack<ItemType>::pop(){
-
+	return head;
 }
 
 template <class ItemType>
-void Stack<ItemType>::push(const ItemType){
-
+void Stack<ItemType>::push(const ItemType element){
+    StackNode<ItemType> s;
+    head = s;
+    s->data = element;
+    s->next = NULL;
+    counter ++;
 }
 
 template <class ItemType>
 ItemType Stack<ItemType>::top() const {
-
+	return head;
 }
 
 template <class ItemType>
 void Stack<ItemType>::clear(){
-
+//    while(!head->isEmpty()) {
+//	pop(head);
+//    }
+//	delete head;
+//	counter = 0;
 }
  
 template <class ItemType>
 bool Stack<ItemType>::isEmpty() const {
+    return (counter == 0);
 
 }
 
 template <class ItemType>
 bool Stack<ItemType>::isFull() const {
+    return (counter == MAX_SIZE);
 
 }
 
 template <class ItemType>
 int Stack<ItemType>::count() const {
-
+	return counter;
  }
--- STACKS/main.cpp
+++ STACKS/main.cpp
@@ -7,12 +7,20 @@
 
 #include <cstdlib>
 
+#include "Stack.h"
+
 using namespace std;
 
 /*
- * 
+ * Stack Manipulation
  */
 int main(int argc, char** argv) {
+
+	Stack<int> s;
+	for (int i = 0; i < 10; i++) {
+	s.push(i);
+	}
+   	s.pop();
 
     return 0;
 }
