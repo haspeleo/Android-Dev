ffc3154063aa99c32a3cc9e0f810ee207d0e9ee1
haspeleo <hassen.doghmen@gmail.com>
2011-04-05 17:28:50 +0200

    algorithms in thrust lib

--- GPGPU/THRUST_LIBRARY/version.cu
+++ GPGPU/THRUST_LIBRARY/version.cu
@@ -3,6 +3,8 @@
  * /usr/local/cuda/include/ 
  * compile using : nvcc  version.cu -o version
  * WARNING: requires cuda 3.2 or greater 
+ *  nvcc version.cu -o version -I /path/to/thrust
+
  */
 #include <thrust/version.h>
 #include <iostream>
