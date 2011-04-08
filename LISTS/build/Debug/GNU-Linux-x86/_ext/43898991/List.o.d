21caf47181379aef56136f12aea3b7272a78317b
haspeleo <hassen.doghmen@gmail.com>
2011-04-08 14:36:43 +0200

    git add LISTS/

---
+++ LISTS/build/Debug/GNU-Linux-x86/_ext/43898991/List.o.d
@@ -0,0 +1,4 @@
+build/Debug/GNU-Linux-x86/_ext/43898991/List.o: ../../List.cpp \
+  ../../List.h
+
+../../List.h:
---
+++ LISTS/main.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.cpp
+ * Author: doghmen
+ *
+ * Created on 8 avril 2011, 14:33
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    cout <<"Class for lists testing"<<endl;
+    return 0;
+}
+
