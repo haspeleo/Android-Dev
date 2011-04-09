5eb8e90c6d3cfa8443e5cc46907b40d48873aee7
haspeleo <hassen.doghmen@gmail.com>
2011-04-09 23:34:15 +0200

    add some methods to heap

--- HEAP/Heap.cpp
+++ HEAP/Heap.cpp
@@ -4,15 +4,36 @@
  * 
  * Created on 8 avril 2011, 17:47
  */
+#include <cstdlib>
 
 #include "Heap.h"
-
-Heap::Heap() {
+template<typename ItemType>
+Heap<ItemType>::Heap() {
+    nbElements = 0;
+}
+template<typename ItemType>
+int Heap<ItemType>::left(int root) const {
+    return (2 * root + 1);
+}
+template<typename ItemType>
+int Heap<ItemType>::right(int root) const {
+    //assert(root < (nbElements - 1) / 2);
+    return (2 * root + 2);
 }
 
-Heap::Heap(const Heap& orig) {
+template<typename ItemType>
+int Heap<ItemType>::parent(int root) const {
+    return (root - 1) / 2;
 }
 
-Heap::~Heap() {
+template<typename ItemType>
+void Heap<ItemType>::insert(const ItemType& item) {
+    //assert(!isFull());
+    array[nbElements] = item;
+    int new_pos = nbElements;
+    nbElements ++;
+    while((new_pos != 0) && (array[new_pos] > array[parent(new_pos)])) {
+	swap(array[new_pos], array[parent(new_pos)]);
+	new_pos = parent(new_pos);
+    }
 }
-
--- HEAP/Heap.h
+++ HEAP/Heap.h
@@ -1,8 +1,9 @@
 /* 
  * File:   Heap.h
  * Author: doghmen
- *
  * Created on 8 avril 2011, 17:47
+ *
+ * http://www.algolist.net/Data_structures/Binary_heap/Array-based_int_repr
  */
 
 #ifndef HEAP_H
@@ -10,16 +11,14 @@
 
 const int MAX_SIZE = 100;
 
-
-
 template<typename ItemType>
 class Heap {
 public:
     Heap();
     virtual ~Heap();
-    int left() const;
-    int right() const;
-    int parent() const;
+    int left(int) const;
+    int right(int) const;
+    int parent(int) const;
     void insert(const ItemType&);
     ItemType removeMax();
     bool isEmpty() const;
@@ -27,7 +26,7 @@ public:
     int count() const;
 private:
     ItemType array[MAX_SIZE];
-    int nbElments;
+    int nbElements;
     void reHeap(int);
 };
 
