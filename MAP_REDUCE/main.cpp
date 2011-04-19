9fa0ea70394583c6d9f3ad3975395fea182396a9
doghmen <doghmen@dell830.(none)>
2011-04-19 10:21:42 +0200

    hadoop tuto

--- MAP_REDUCE/main.cpp
+++ MAP_REDUCE/main.cpp
@@ -1,19 +1,3 @@
-/* 
- * File:   main.cpp
- * Author: doghmen
- *
- * Created on April 18, 2011, 8:45 PM
- */
-
-#include <cstdlib>
-
-using namespace std;
-
 /*
- * 
+ * http://wiki.apache.org/hadoop/
  */
-int main(int argc, char** argv) {
-
-    return 0;
-}
-
