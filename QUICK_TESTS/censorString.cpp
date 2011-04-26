0230315f73aae5609c3291b27e20d5ed3c03b004
doghmen <doghmen@dell830.(none)>
2011-04-26 06:23:17 +0200

    delete characters from a word -- version 1

--- QUICK_TESTS/censorString.cpp
+++ QUICK_TESTS/censorString.cpp
@@ -1,5 +1,3 @@
-
-
 /*
  *http://technical-interview.com/C_Questions_1.aspx
  * */
@@ -10,66 +8,36 @@ using namespace std;
 
 string censorString(string text, string remove) {
 
-      string result = "";
-      for (int i = 0; i < text.length(); i++)
-      {
-            bool found = false;
-            for (int k = 0; k < remove.length(); k++)
-            {
-                  if (text[i] == remove[k])
-                  {
-                        found = true;
-                        break;
-                  }
-            }
-            if(!found)
-            {
-                  result += text[i];
-            }
-      }
-      return result;
+	string result = "";
+	for(int i = 0; i < text.length(); i++) {
+		bool found = false;
+		for(int j = 0; j < remove.length(); j++) {
+			if(text[i] == remove[j]) {
+				found = true;
+				break;
+			}
+		}
+	if(!found) {
+		result += text[i];
+	}
+
+
+} 	
+
+return result;
 }
 
 
-
-
-
-/*
-    string str = "";
-    int textSize = text.length();
-    int removeSize = remove.length();
-
-    for(int i = 0; i < textSize; i++) {
-
-        int j = 0; //removeSize;
-                cout<<"i = "<<i<<endl;
-                cout<<"j = "<<j<<endl;
-        while(j < removeSize) { //> 0) {
-            if(text[i] == remove[j]) 
-                j = removeSize + 1;
-                else
-                j ++; //--;
-        }
-
-        if(j < removeSize) //j <= 0)
-        str += text[i];
-       }
-return str;
-}
-*/
 
 int main() {
 
-string  text = "stanford";
-string remove = "nt";
-
-string output = censorString(text, remove);
-
-cout <<"After censoring : "<<output<<endl;
-
-
-
-
+	string  text = "stanford";
+	string remove = "nt";
+	
+	string output = censorString(text, remove);
+	
+	cout <<"After censoring : "<<output<<endl;
+	
 return 0;
 
 }
