62ff09e57567a8ef1c96ed0476f77de0a89a1fcb
haspeleo <hassen.doghmen@gmail.com>
2011-05-01 19:44:48 +0200

    helloworld cmake

---
+++ CMAKE/helloworld/build/src/CMakeFiles/helloworld.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/helloworld.dir/helloworld.cpp.o"
+  "CMakeFiles/helloworld.dir/main.cpp.o"
+  "helloworld.pdb"
+  "helloworld"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/helloworld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
