024cb00568d4c8e37a644ee404c368878dcfad55
haspeleo <hassen.doghmen@gmail.com>
2011-05-01 20:54:10 +0200

    build automation with cmake

---
+++ makefile/C++_example/simpleExample_0_cmake/build/src/CMakeFiles/cpp_simple.dir/DependInfo.cmake
@@ -0,0 +1,15 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/users/tao/doghmen/interviewing/makefile/C++_example/simpleExample_0_cmake/src/cpp_simple.C" "/users/tao/doghmen/interviewing/makefile/C++_example/simpleExample_0_cmake/build/src/CMakeFiles/cpp_simple.dir/cpp_simple.C.o"
+  "/users/tao/doghmen/interviewing/makefile/C++_example/simpleExample_0_cmake/src/f.C" "/users/tao/doghmen/interviewing/makefile/C++_example/simpleExample_0_cmake/build/src/CMakeFiles/cpp_simple.dir/f.C.o"
+  "/users/tao/doghmen/interviewing/makefile/C++_example/simpleExample_0_cmake/src/midpoint.C" "/users/tao/doghmen/interviewing/makefile/C++_example/simpleExample_0_cmake/build/src/CMakeFiles/cpp_simple.dir/midpoint.C.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ makefile/C++_example/simpleExample_0_cmake/build/src/CMakeFiles/cpp_simple.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/cpp_simple.dir/cpp_simple.C.o"
+  "CMakeFiles/cpp_simple.dir/midpoint.C.o"
+  "CMakeFiles/cpp_simple.dir/f.C.o"
+  "cpp_simple.pdb"
+  "cpp_simple"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/cpp_simple.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
