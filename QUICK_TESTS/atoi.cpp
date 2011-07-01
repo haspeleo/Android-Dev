841b4c3e2c28f9fc11bb42cb8df9d6c70a54517e
haspeleo <hassen.doghmen@gmail.com>
2011-07-01 17:06:04 +0200

    parsing arguments

--- QUICK_TESTS/atoi.cpp
+++ QUICK_TESTS/atoi.cpp
@@ -1,6 +1,8 @@
 
 /*
- * http://www.site.uottawa.ca/~lucia/courses/2131-05/labs/Lab3/CommandLineArguments.html
+ * http://www.site.uottawa.ca/~lucia/courses/2131-05/labs/Lab3/CommandLineArguments.html -- command line arguments
+ * http://www.cplusplus.com/forum/articles/13355/ -- parsing command line parameters
+ * http://www.gnu.org/s/libc/manual/html_node/Getopt.html  --gnu 
  */
 
 
@@ -9,12 +11,22 @@
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
+//int main(int argc, char** argv) {   /* equivalent form of main */
+
+int main(int argc, char* argv[]) {
+
+
+    cout << "argc = " << argc << endl; 
+
+   for(int i = 0; i < argc; i++) {
+   cout << "argv[" << i << "] = " << argv[i] << endl;
+}
 
 int x = atoi(argv[1]);
 int y = atoi(argv[2]);
 
 cout <<x<<y<<endl;
+
 
 return 0;
 }
